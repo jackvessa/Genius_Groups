cc64f0ba5041d65d738a4da60bd61a66f1a58df7
Jack Vessa <jack.k.vessa@vanderbilt.edu>
2019-10-17 14:53:29 -0700

    Updating Main.py

--- main.py
+++ main.py
@@ -318,7 +318,7 @@ def cluster_all():
                 groups_string += "<br/>Cluster " + str(i+1) + ":<br/>"
                 groups_string += str(list(val)) + "<br/>"
                 groups_string += ("Average Score: " +
-                    str(round(np.mean(student_df.loc[val]['Current Score']),2)) +"%<br/>")
+                    str(round(np.mean(student_df.loc[val]['Current Score'])*100,2)) +"%<br/>")
 
             return '''
                 <html>
