b80a452894828a54202f6dd7445f155980c03387
Jack Vessa <jack.k.vessa@vanderbilt.edu>
2019-10-17 23:36:57 -0700

    Updating Functions

--- Smart_Group_Functions.py
+++ Smart_Group_Functions.py
@@ -8,6 +8,7 @@ from sklearn.cluster import KMeans
 # num_groups = 3
 # Homogenous = True
 
+# Create Group Sizes
 def calc_group_sizes(num_students, num_groups):
     '''
     Parameters
@@ -35,7 +36,7 @@ def calc_group_sizes(num_students, num_groups):
 
     return group_sizes
 
-
+# Clean DataFrame by Section
 def clean_file(dataframe,sectionID):
     '''
     Clean CSV file
@@ -64,7 +65,7 @@ def clean_file(dataframe,sectionID):
 
     return df
 
-
+# Normalize DataFrame (0-1)
 def normalize_df(df):
     '''
     Normalize DataFrame Values from 0-1
@@ -80,7 +81,7 @@ def normalize_df(df):
     return df.apply(lambda x: (x - np.min(x)) / (np.max(x) - np.min(x)) \
         if np.min(x) != np.max(x) else x)
 
-
+# Generate Optimized Group (Based on Residual Sum of Squares)
 def generate_optimized_groups(student_df, num_iter = 100, num_groups = 6, Homogeneous = 0, criteria = 'score'):
     '''
 
@@ -151,7 +152,7 @@ def generate_optimized_groups(student_df, num_iter = 100, num_groups = 6, Homoge
 
     return best_group
 
-
+# Clean File Based on All Gradebook Assignments and Section
 def clean_file_all_assignments(dataframe,sectionID):
     '''
     Clean CSV file
@@ -190,7 +191,7 @@ def clean_file_all_assignments(dataframe,sectionID):
 
     return class_df
 
-
+# Add Cluster Column to DataFrame
 def add_clusters(df, num_clusters=6):
     '''
     Add Clusters
@@ -201,7 +202,7 @@ def add_clusters(df, num_clusters=6):
     df['Cluster'] = cluster
     return df
 
-
+# Return a list of Clusters
 def return_cluster_list(df,num_clusters=6):
     '''
     Return a List of Clustered Students
@@ -212,6 +213,180 @@ def return_cluster_list(df,num_clusters=6):
         cluster_list.append(list(df[df['Cluster']==i].index))
 
     return cluster_list
+
+
+## NLP Clustering of Questions
+
+# Create a List of Question Strings
+def create_questions_list(df, cols=['id','sis_id','section_id','section_sis_id','submitted','attempt','score','name','section']):
+    '''
+    Takes DataFrame and creates a NumPy array.
+
+    Parameters
+    ----------
+    df : Pandas DataFrame
+    cols : list of columns to drop from DataFrame
+
+    Returns
+    -------
+    List of Questions
+    '''
+    df.drop(columns=cols,inplace=True)
+    df = df.select_dtypes(exclude=['float'])
+    df = df.select_dtypes(exclude=['int'])
+    column_name_list = np.array(list(df.columns))
+    quest_list = []
+    for quest in column_name_list:
+        quest_list.append(quest[6:])
+    return np.array(quest_list)
+
+
+# Group Questions into Clusters
+def cluster_question_topics(quest_list,num_clust = 3,num_top_words = 3,stop_words = ["q"], max_features = 50):
+    '''
+    Create clusters of questions based on word usage
+
+    Parameters
+    ----------
+    quest_list : list of questions
+    num_clust : number of question clusters to create
+    num_top_words : number of words to display in result
+    stop_words : list of words to remove from clustering
+    max_features : number of words to include in clustering (top X by frequency)
+
+    Returns
+    -------
+    String of results
+    Labels of Clustered Questions
+    '''
+
+#     my_stop_words = text.ENGLISH_STOP_WORDS.union(["ae","word","exam","alphabetical","options","order",
+#                                         "things","used",'________','called','likely','provides','provide',
+#                                         '_____','10','correct','match','12','14','32777','isa','thing','11',
+#                                         '13','sentence','definition','suffix','suffixes','prefix','prefixes',
+#                                         'uses','terms','means','following','function','specialist',
+#                                         'position','correctly','directional','body','anatomical','plane',
+#                                         'means_________'])
+
+    my_stop_words = text.ENGLISH_STOP_WORDS.union(stop_words)
+
+    vectorizer = TfidfVectorizer(strip_accents='ascii',stop_words=my_stop_words,max_features=max_features)
+    X = vectorizer.fit_transform(quest_list)
+    means = KMeans(num_clust,).fit(X)
+
+    print("Top terms per cluster:")
+    order_centroids = means.cluster_centers_.argsort()[:, ::-1]
+    terms = vectorizer.get_feature_names()
+
+    string_results = ""
+    for i in range(num_clust):
+        print("\nCluster {}:".format(str(i+1)))
+        string_results += ("\nCluster {}:".format(str(i+1)))
+        for ind in order_centroids[i, :num_top_words]:
+            print(' %s' % terms[ind])
+            string_results += (' %s' % terms[ind])
+    print(means.labels_)
+
+    return (string_results, np.array(means.labels_))
+
+
+# Make Student Strength/Growth Areas DataFrame
+def make_student_growth_and_strength_df(df,sectionID,cluster_labels):
+    '''
+    Create a DataFrame that includes students strengths and growth areas for question clusters
+
+    Return a List of Clustered Students
+
+    Parameters
+    ----------
+    df : Pandas DataFrame
+    sectionID : Section Number for Students
+
+    Returns
+    -------
+    Pandas DataFrame with strengths and growth areas for question clusters
+
+    '''
+    clean_df = clean_file(df,sectionID)
+    quest_num_df = clean_df.iloc[:,0:-3]
+    quest_num_df.columns = cluster_labels
+    quest_num_df_grouped = quest_num_df.groupby(quest_num_df.columns, axis=1).sum()
+    grouped_quest_normed_df = normalize_df(quest_num_df_grouped)
+
+    x = grouped_quest_normed_df.copy().ix[0]
+    x.index[x.argmin()]
+    min_list, max_list = [], []
+
+    for i in range (len(grouped_quest_normed_df)):
+        x = grouped_quest_normed_df.ix[i]
+
+        min_list.append(x.index[x.argmin()])
+        max_list.append(x.index[x.argmax()])
+
+
+    grouped_quest_normed_df['Strength Area'] = max_list
+    grouped_quest_normed_df['Growth Area'] = min_list
+
+    return grouped_quest_normed_df
+
+
+# Generate Growth Areas Groups
+def generate_growth_groups(df,num_groups):
+    '''
+
+    Parameters
+    ----------
+    student_df : DataFrame with student names as index and Strength/Growth Areas included
+
+    Returns
+    -------
+    Growth Area Groups
+    '''
+    index_list = list(df.index)
+
+    cluster_focus = []
+
+    for i in range(num_groups):
+
+        cluster_focus.append(list(df[df['Growth Area']==i].index))
+
+    print("Grouping by Growth Areas:\n")
+
+    for i,g in enumerate(cluster_focus):
+        print("Group",i+1)
+        print(str(g)+"\n")
+
+    return cluster_focus
+
+
+# Generate Strength Areas Groups
+def generate_strength_groups(df,num_groups):
+    '''
+
+    Parameters
+    ----------
+    student_df : DataFrame with student names as index and Strength/Growth Areas included
+
+    Returns
+    -------
+    Growth Area Groups
+    '''
+    index_list = list(df.index)
+
+    cluster_focus = []
+
+    for i in range(num_groups):
+
+        cluster_focus.append(list(df[df['Strength Area']==i].index))
+
+    print("Grouping by Strength Areas:\n")
+
+    for i,g in enumerate(cluster_focus):
+        print("Group",i+1)
+        print(str(g)+"\n")
+
+    return cluster_focus
+ 
 
 
 if __name__ == "__main__":
